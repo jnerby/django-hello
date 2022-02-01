f24a24337c071632d6162f94ce4c4615e3ed0ccd
Jen Nerby <nerby.jennifer@gmail.com>
2022-02-01 14:55:12 -0700

    added hello/index.html

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     path("", views.index, name="index"),
-    path("jen", views.jen, name="jen")
+    path("jen", views.jen, name="jen"),
+    path("<str:name>", views.greet, name="greet")
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,12 @@
+from curses.ascii import HT
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def jen(request):
     return HttpResponse("Hello, Jen!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
